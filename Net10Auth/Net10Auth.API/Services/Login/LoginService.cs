bb697a87e6d0ce8474fd12be488d07b9cf82d3a1
bartvanhoey <bartvanhoey@hotmail.com>
2025-07-05 15:43:06 +0200

    Update

--- Net10Auth/Net10Auth.API/Services/Login/LoginService.cs
+++ Net10Auth/Net10Auth.API/Services/Login/LoginService.cs
@@ -8,40 +8,47 @@ using Net10Auth.Shared.Models.Identity;
 
 namespace Net10Auth.API.Services.Login;
 
-public class LoginService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration, IOptions<JwtConfiguration> jwtConfiguration) : ILoginService
+public class LoginService(
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager,
+    IConfiguration configuration,
+    IOptions<JwtConfiguration> jwtConfiguration) : ILoginService
 {
     public async Task<CtrlResponse<LoginResponse>> LoginAsync(LoginInputModel model, HttpRequest httpRequest)
     {
-        // var validationResult = ValidateControllerInputModel(model, logger, nameof(Login));
-        // if (validationResult.IsFailure) return Nok500<LoginResponse>(logger, validationResult.Error);
-
         var user = await userManager.FindByEmailAsync(model.Email);
         if (user == null) return new CtrlResponse<LoginResponse>("user not found");
-        
+
         if (user.Email.IsNullOrWhiteSpace()) return new CtrlResponse<LoginResponse>("Email is null");
-        
-             
-         var signInResult = await signInManager.CheckPasswordSignInAsync(user, model.Password ?? throw new InvalidOperationException(),  true);
-        if (signInResult is { Succeeded: false, IsLockedOut: true } ) return new CtrlResponse<LoginResponse>("user is locked out");
-             
+
+        var signInResult =
+            await signInManager.CheckPasswordSignInAsync(user, model.Password ?? throw new InvalidOperationException(),
+                true);
+        if (signInResult is { Succeeded: false, IsLockedOut: true })
+            return new CtrlResponse<LoginResponse>("user is locked out");
+
         if (!signInResult.Succeeded) return new CtrlResponse<LoginResponse>("Invalid login attempt");
-        
+
         user.SetRefreshToken(configuration);
-        
+
         await userManager.UpdateAsync(user);
-        
-        var (accessToken, validTo) = 
-            await user.GenerateAccessToken(userManager,  jwtConfiguration.Value, httpRequest);
-        //
-        // if (user.RefreshToken == null || user.RefreshToken.IsNullOrWhiteSpace())
-        //     return Nok500RefreshToken<LoginResponse>(logger);
-        //     
-        // if (accessToken.IsNullOrWhiteSpace()) return Nok500<LoginResponse>(logger, "AccessToken is null or whitespace");
-        // if (validTo <= DateTime.UtcNow) return Nok500<LoginResponse>(logger, "ValidTo is in the past" );
-        //
-        // return Ok(user.TwoFactorEnabled 
-        //     ? new LoginResponse(accessToken, user.RefreshToken, validTo, user.TwoFactorEnabled, user.Id) 
-        //     : new LoginResponse(accessToken, user.RefreshToken, validTo, user.TwoFactorEnabled));
-        return new CtrlResponse<LoginResponse>("");
+
+        var (accessToken, validTo) =
+            await user.GenerateAccessToken(userManager, jwtConfiguration.Value, httpRequest);
+
+        if (user.RefreshToken == null || user.RefreshToken.IsNullOrWhiteSpace())
+            return new CtrlResponse<LoginResponse>("refresh token is null");
+
+        if (accessToken.IsNullOrWhiteSpace())
+            return new CtrlResponse<LoginResponse>("AccessToken is null or whitespace");
+
+        if (validTo <= DateTime.UtcNow)
+            return new CtrlResponse<LoginResponse>("ValidTo is in the past");
+
+        return user.TwoFactorEnabled
+            ? new CtrlResponse<LoginResponse>(new LoginResponse(accessToken, user.RefreshToken, validTo,
+                user.TwoFactorEnabled, user.Id))
+            : new CtrlResponse<LoginResponse>(new LoginResponse(accessToken, user.RefreshToken, validTo,
+                user.TwoFactorEnabled));
     }
 }
--- Net10Auth/Net10Auth.Shared/ControllerResponses/LoginResponse.cs
+++ Net10Auth/Net10Auth.Shared/ControllerResponses/LoginResponse.cs
@@ -2,38 +2,16 @@
 
 namespace Net10Auth.Shared.ControllerResponses;
 
-public class LoginResponse 
+public class LoginResponse(
+    string? accessToken,
+    string refreshToken,
+    DateTime validTo,
+    bool twoFactorEnabled,
+    string? userId = null)
 {
-    public LoginResponse()
-    {
-        // do not remove
-    }
-
-    public LoginResponse(string? accessToken, string refreshToken, DateTime validTo, bool twoFactorEnabled, string? token = null) : this()
-    {
-        AccessToken = accessToken;
-        RefreshToken = refreshToken;
-        ValidTo = validTo;
-        TwoFactorEnabled = twoFactorEnabled;
-        Token = token;
-    }
-
-    public string? AccessToken { get; set; }
-    public string? RefreshToken { get; set; }
-    public DateTime ValidTo { get; set; }
-    public bool TwoFactorEnabled { get; set; }
-    public string? Token { get; set; }
-
-
-}
-
-public class ControllerResponseError
-{
-}
-
-public interface IControllerResponse
-{
-    public int StatusCode { get; set; }
-    public string? Message { get; set; }
-    public IEnumerable<ControllerResponseError>? Errors { get; set; }
+    public string? AccessToken { get; set; } = accessToken;
+    public string? RefreshToken { get; set; } = refreshToken;
+    public DateTime ValidTo { get; set; } = validTo;
+    public bool TwoFactorEnabled { get; set; } = twoFactorEnabled;
+    public string? UserId { get; set; } = userId;
 }
